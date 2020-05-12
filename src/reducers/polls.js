34d6e3ccff97a3205428dee74a2d1be1cbe24b40
Syed Affan Hamdani <syedaffanhamdani@Syeds-MBP.fritz.box>
2020-05-13 01:53:49 +0200

    added poll reducer

---
+++ src/reducers/polls.js
@@ -0,0 +1,30 @@
+import { RECEIVE_INTIAL_QUESTIONS, ADD_POLL_QUESTION, SAVE_POLL_ANSWER } from '../actions/pollingactions'
+
+export default function questions(state = {}, action) {
+    switch (action.type) {
+        case RECEIVE_INTIAL_QUESTIONS:
+            return {
+                ...state,
+                ...action.questions
+            }
+        case SAVE_POLL_ANSWER:
+            const { authenticatedUser, questionId, answer } = action
+            return {
+                ...state,
+                [questionId]: {
+                    ...state[questionId],
+                    answer: {
+                        ...state[questionId][answer],
+                        votes: state[questionId][answer].votes.concat([authenticatedUser])
+                    }
+                }
+            }
+            case ADD_POLL_QUESTION:
+                return {
+                    ...state,
+                    [action.poll.id]: action.poll
+                }
+        default:
+            return state
+    }
+}
