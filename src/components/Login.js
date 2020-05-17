6b6cfb10da45207b68c5dfa7c9c733b94031efc7
Syed Affan Hamdani <syedaffanhamdani@Syeds-MBP.fritz.box>
2020-05-17 17:51:30 +0200

    added login jumbotron

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import {setAuthenticatedUser} from '../actions/authenticatedUser'
-
+import { setAuthenticatedUser } from '../actions/authenticatedUser'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 class Login extends Component {
 
     state = {
-        selectedUser : '',
+        selectedUser: '',
     }
 
-    onUserSelected = (selectedUser) => this.setState({selectedUser})
+    onUserSelected = (selectedUser) => this.setState({ selectedUser })
 
     handleSubmit = (e) => {
         e.preventDefault()
-        const {selectedUser} = this.state
-        const {setAuthenticatedUser} = this.props
-        if(selectedUser){
+        const { selectedUser } = this.state
+        const { setAuthenticatedUser } = this.props
+        if (selectedUser) {
             setAuthenticatedUser(selectedUser)
         }
         else {
@@ -23,27 +24,34 @@ class Login extends Component {
     }
 
     render() {
-        const {users} = this.props
-        const {selectedUser} = this.state
-    
+        const { users } = this.props
+       // const { selectedUser } = this.state
+
         return (
             <div className="loginContainer">
-            <form id="Login" onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                    <select className="form-control" id="userId" onChange={(e)=>this.onUserSelected(e.target.value)}>
-                        <option>Select User to Login</option>
-                        {users && 
-                            Object.keys(users).map(
-                                user1 => <option className="hbsc" key={user1} value={user1}>{user1}
+                <Container className="p-3">
+                    <Jumbotron>
+                        <h2 className="header">Select a user to login</h2>
 
-                                </option>
-                            )
-                        }
-                    </select>
-                </div>
+                        <form id="Login" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <select className="form-control" id="userId" onChange={(e) => this.onUserSelected(e.target.value)}>
+                            <option>Select User to Login</option>
+                            {users &&
+                                Object.keys(users).map(
+                                    user1 => <option className="hbsc" key={user1} value={user1}>{user1}
 
-                <button type="submit" className="btn btn-primary" disabled={this.state.userSelected === ''}>Login</button>
-            </form>
+                                    </option>
+                                )
+                            }
+                        </select>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary" disabled={this.state.userSelected === ''}>Login</button>
+                </form>
+                    </Jumbotron>
+                </Container>
+
             </div>
         );
     }
@@ -51,7 +59,7 @@ class Login extends Component {
 
 }
 
-function mapStateToProps ({ users }) {
+function mapStateToProps({ users }) {
     return {
         users
     }
@@ -67,4 +75,4 @@ function mapDispatchToProps(dispatch) {
 
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
